862689131375642c5518288def16ca9ec7d3fe5d
Artur Siembab <ajtuss@gmail.com>
2018-08-31 15:47:13 +0200

    add entities

---
+++ src/main/java/pl/coderslab/tweeter/enities/Comment.java
@@ -0,0 +1,95 @@
+package pl.coderslab.tweeter.enities;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Tweet tweet;
+
+    @Column(columnDefinition = "TEXT")
+    private String text;
+
+    private LocalDateTime created;
+
+    @PrePersist
+    public void setCreateDate(){
+        created = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Tweet getTweet() {
+        return tweet;
+    }
+
+    public void setTweet(Tweet tweet) {
+        this.tweet = tweet;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                ", created=" + created +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comment comment = (Comment) o;
+        return Objects.equals(id, comment.id) &&
+                Objects.equals(text, comment.text) &&
+                Objects.equals(created, comment.created);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, text, created);
+    }
+}
---
+++ src/main/java/pl/coderslab/tweeter/enities/Message.java
@@ -0,0 +1,107 @@
+package pl.coderslab.tweeter.enities;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "messages")
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User sender;
+
+    @ManyToOne
+    private User receiver;
+
+    @Column(columnDefinition = "TEXT")
+    private String text;
+
+    private Boolean readed=false;
+
+    private LocalDateTime created;
+
+    @PrePersist
+    public void setCreateDate(){
+        created = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Boolean getReaded() {
+        return readed;
+    }
+
+    public void setReaded(Boolean readed) {
+        this.readed = readed;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Message message = (Message) o;
+        return Objects.equals(id, message.id) &&
+                Objects.equals(text, message.text) &&
+                Objects.equals(readed, message.readed) &&
+                Objects.equals(created, message.created);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, text, readed, created);
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                ", readed=" + readed +
+                ", created=" + created +
+                '}';
+    }
+}
